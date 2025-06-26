65144acecf8c808983fe9a1777f278c53ef98d20
brob92631 <brob9263@gmail.com>
2025-06-26 15:35:09 +0200

    Update AuthContext.tsx

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import type { User } from '@supabase/supabase-js';
