bd8a1d50e2f95aeb01f9838f9265e06079c37035
brob92631 <brob9263@gmail.com>
2025-06-26 17:14:36 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import { ReactNode } from 'react';
 import { AuthProvider } from '../context/AuthContext';
+import { AudioPlayerProvider } from '../context/AudioPlayerContext';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <AudioPlayerProvider>
+            {children}
+          </AudioPlayerProvider>
+        </AuthProvider>
       </body>
     </html>
   );
