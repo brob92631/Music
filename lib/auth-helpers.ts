89b7accddb129d7ca116bc7198bb488759417654
brob92631 <brob9263@gmail.com>
2025-06-26 17:44:26 +0200

    Update auth-helpers.ts

--- lib/auth-helpers.ts
+++ lib/auth-helpers.ts
@@ -5,7 +5,7 @@ export async function getUser() {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    { cookies }
+    { cookies: cookies() }
   )
 
   const { data: { user } } = await supabase.auth.getUser()
